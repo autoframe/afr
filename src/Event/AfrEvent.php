cd123522e1772be319fce8c432cd4293e001b808
Nistor Alexandru Marius <djthorr@gmail.com>
2025-01-25 00:21:31 +0200

    daily

--- src/Event/AfrEvent.php
+++ src/Event/AfrEvent.php
@@ -2,18 +2,22 @@
 
 namespace Autoframe\Core\Event;
 
-//TODO de mergiuit cu clasa de log pt ury ca am acolo timings
+//TODO : integrare ddtrace https://docs.datadoghq.com/tracing/trace_collection/custom_instrumentation/php/dd-api?tab=currentspan
+
 use Autoframe\Core\Afr\Afr;
-use Autoframe\Core\Env\Exception\AfrEnvException;
 use Autoframe\Core\Event\Exception\AfrEventException;
 use Autoframe\Core\String\Obj\AfrClosureToStr;
+use Autoframe\Core\Tenant\AfrDefaultTenantConfigsInterface;
+use Autoframe\Core\Tenant\AfrTenant;
 use Throwable;
 use Closure;
 
-class AfrEvent
+/**
+ * Class AfrEvent
+ */
+class AfrEvent implements AfrDefaultTenantConfigsInterface
 {
-//	const AFR_EVENT = 'afr.event'; //applies on any event
-//	const AFR_EXCEPTION = 'afr.exception';
+	const AFR_EXCEPTION = 'afr.exception';
 	const AFR_BOOTSTRAP = 'afr.bootstrap';
 	const AFR_RUN = 'afr.run';
 	const DB_ACTION_START = 'db.action.start0';
@@ -52,65 +56,125 @@ class AfrEvent
 	const CACHE_HIT = 'cache.hit';
 	const CACHE_MISS = 'cache.miss';
 
-	const X_EVT = 'e';
-	const X_ARGS = 'a';
-	const X_TRACE = 't';
-	const X_TIMING = 'm';
-	const X_WILDCARDS = 'w';
+	const X_EVT = 'evt';
+	const X_ARGS = 'args';
+	const X_TRACE = 'trace';
+	const X_TIMING = 'rqt';
+	const X_EVENT_TIME = 'et';
+	const X_DELTA_TIME = 'delta';
+	const X_WILDCARDS = 'wildcards';
+	const X_CLOSURE_RESULTS = 'res';
+	const X_MEMORY_MB = 'mem';
 
-
-	const  WILDCARD_STARTS_WITH = 'afr.*'; //applies on any event starting with afr.*
-	const  WILDCARD_ENDS_WITH = '*afr'; //applies on any event ending with *afr
-	const  WILDCARD_CONTAINS = '*afr*'; //applies on any event containing the substring
+	const  WILDCARD_STARTS_WITH = 'str*'; //applies on any event starting with afr.*
+	const  WILDCARD_ENDS_WITH = '*str'; //applies on any event ending with *afr
+	const  WILDCARD_CONTAINS = '*str*'; //applies on any event containing the substring
 	const  WILDCARD_STAR = '*'; //applies on any event
 
 	public static bool $bSlimTrace = true;
-	public static int $iTraceDepth = 5;
+	public static ?array $aPrevTrace = null;
+	public static int $iDefaultTraceDepth = 5;
 
-	public static ?bool $bHrTime = false;
+	/**
+	 * Bool means on or off, else int is the chance to trigger rand(0,x)<1
+	 * Set to 99 for a 1% trigger chance
+	 * @var bool|int
+	 */
+	public static $mMemoryUsage = false;
+
+	protected static array $aOnEventClosures = [];
+	protected static array $aWildcardClosure = [];
 
 	protected static array $aTriggeredEventsStats = [];
-	public static array $aTriggeredEventsResults = [];
-	protected static array $aOnEventClosures = [];
-	protected static array $aWildcardEvents = [];
+	public static array $aDataCollectFlags = [ //TODO load from tenant and defaults, done acolo, nu aici!
+		'Connection\AfrDbConnectionManagerClass::getInstance' => [self::X_ARGS => false, self::X_TRACE => 5],
+		//	'SomeEvD' => [self::X_ARGS => true, self::X_TRACE => 7],
+		'*Last*' => [self::X_ARGS => true, self::X_TRACE => 0],
+	];
+	protected static bool $bDefaultTenantConfigWasApplied = false;
 
+
+	public static function sampleTenantDefaultConfig(): ?string
+	{
+		return file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'config.sample.AfrEvent.php');
+	}
+
+	public static function applyDefaultTenantConfig(): void
+	{
+		if (!empty(static::$bDefaultTenantConfigWasApplied)) {
+			return;
+		}
+		if (!empty($sConfigFilePath = AfrTenant::getAfrDefaultTenantConfigsForFqcn(static::class))) {
+			static::$bDefaultTenantConfigWasApplied = true;
+			if (file_exists($sConfigFilePath)) {
+				static::extendConfigFlagsFromArray((include $sConfigFilePath));
+			}
+		}
+	}
 
 	/**
 	 * @param string $sEvent
 	 * @param array|null $aArgs
 	 * @param int|null $iTraceDepth
+	 * @param bool|null $mMemoryUsage
 	 * @return array
 	 * @throws AfrEventException
 	 */
 	public static function dispatchEvent(
-		string $sEvent,
+		string $sEvent = '',
 		array  $aArgs = null,
-		int    $iTraceDepth = null
+		int    $iTraceDepth = null,
+		bool   $mMemoryUsage = null
 	): array
 	{
-		$sEvent = $sEvent ?: static::autoEventName(3);
-		$iTraceDepth = self::getTraceDepth($iTraceDepth, 'EV_TRACE_DEPTH_' . $sEvent);
+		if (empty(self::$aTriggeredEventsStats)) {
+			self::$aTriggeredEventsStats[] = [
+				self::X_EVT => 'REQUEST_TIME_FLOAT',
+				self::X_TIMING => $_SERVER['REQUEST_TIME_FLOAT'] ?? microtime(true),
+				self::X_EVENT_TIME => 0.0,
+				self::X_DELTA_TIME => 0.0,
+			];
+		}
 
-		$aTrace = $iTraceDepth ?
-			self::slimDebugTrace(
-				debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, $iTraceDepth),
-				self::$bSlimTrace
-			) : null;
-
+		self::$aPrevTrace = null;
+		$sEvent = $sEvent ?: static::autoEventName(
+			2,
+			(self::$aPrevTrace = debug_backtrace(1, 2))
+		);
 		$aData = [
 			self::X_EVT => $sEvent,
-			self::X_TIMING => static::hrMicroTime(),
+			//	self::X_TIMING => microtime(true),
+			self::X_EVENT_TIME => $t = (microtime(true) - self::$aTriggeredEventsStats[0][self::X_TIMING]) * 1000,
+			self::X_DELTA_TIME => $t - end(self::$aTriggeredEventsStats)[self::X_EVENT_TIME],
 		];
-		if ($aArgs) {
-			$aData[self::X_ARGS] = $aArgs;
+
+		if ($mMemoryUsage || self::$mMemoryUsage === true || is_integer(self::$mMemoryUsage) && rand(0, self::$mMemoryUsage) < 1) {
+			$aData[self::X_MEMORY_MB] = round((memory_get_usage() / (1024 * 1024)), 2);
 		}
-		if ($aTrace) {
-			$aData[self::X_TRACE] = $aTrace;
+
+		if (static::logArgs($sEvent)) {
+			if ($aArgs === null) { //auto if configured from config flag or env
+				self::$aPrevTrace ??= debug_backtrace(1, 2);
+				$aArgs = self::$aPrevTrace[1]['args'] ?? null;
+			}
+			if ($aArgs !== null) {
+				$aData[self::X_ARGS] = $aArgs;
+			}
+		}
+		if (($iTraceDepth = static::getTraceDepth($iTraceDepth, $sEvent))) {
+			$aData[self::X_TRACE] = static::slimDebugTrace(
+				empty(self::$aPrevTrace) || $iTraceDepth > 2 ?
+					(self::$aPrevTrace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, $iTraceDepth)) :
+					self::$aPrevTrace,
+				self::$bSlimTrace
+			);
 		}
 		$aResults = self::callEventClosure($sEvent, $aData, []);
 
+		$X_WILDCARDS = !empty(self::$aWildcardClosure) &&
+			(self::getEnv('AFR_EVENT_X_COLLECT_WILDCARDS_INFO', false) || self::getConfigFlag($sEvent, self::X_WILDCARDS));
 		//wildcards
-		foreach (self::$aWildcardEvents ?? [] as $sWildcardEvent => $aHow) {
+		foreach (self::$aWildcardClosure ?? [] as $sWildcardEvent => $aHow) {
 			$bTrigger = ($sWildcardEvent === self::WILDCARD_STAR);
 			if (!$bTrigger && !empty($aHow[self::WILDCARD_STARTS_WITH]) && $aHow[self::WILDCARD_STARTS_WITH][0] ===
 				substr($sEvent, 0, $aHow[self::WILDCARD_STARTS_WITH][1])) {
@@ -131,16 +195,27 @@ class AfrEvent
 			if (!$bTrigger) {
 				continue;
 			}
-			$aData[self::X_WILDCARDS] ??= [];
-			$aData[self::X_WILDCARDS][] = [
-				self::X_EVT => $sWildcardEvent,
-				self::X_TIMING => static::hrMicroTime(),
-			];
+
 			$aResults = self::callEventClosure($sWildcardEvent, $aData, $aResults);
+			if ($X_WILDCARDS) {
+				$aData[self::X_WILDCARDS] ??= [];
+				$aData[self::X_WILDCARDS][] = [
+					self::X_EVT => $sWildcardEvent,
+					//	self::X_TIMING => microtime(true),
+					self::X_EVENT_TIME => $t = (microtime(true) - self::$aTriggeredEventsStats[0][self::X_TIMING]) * 1000,
+					self::X_DELTA_TIME => $t - end(self::$aTriggeredEventsStats)[self::X_EVENT_TIME],
+
+				];
+			}
 		}
 
-		self::$aTriggeredEventsStats[] = $aData;
-		return self::$aTriggeredEventsResults[] = $aResults;
+
+		if (self::getConfigFlag($sEvent, self::X_CLOSURE_RESULTS) || self::getEnv('AFR_EVENT_X_CLOSURE_RESULTS', false)) {
+			$aData[self::X_CLOSURE_RESULTS] = $aResults;
+		}
+
+		self::$aPrevTrace = null;
+		return self::$aTriggeredEventsStats[] = $aData;
 	}
 
 
@@ -151,7 +226,7 @@ class AfrEvent
 	 * @return array
 	 * @throws AfrEventException
 	 */
-	protected static function callEventClosure(string $sEvent, array $aData, array $aResults): array
+	protected static function callEventClosure(string $sEvent, array &$aData, array $aResults): array
 	{
 		$oScopeInstance = null;
 		foreach (self::$aOnEventClosures[$sEvent] ?? [] as $i => $aClosureAndScopeBound) {
@@ -159,13 +234,11 @@ class AfrEvent
 			[$closure, $bInnerBoundScope, $newScope] = $aClosureAndScopeBound;
 			if ($bInnerBoundScope) {
 				if ($oScopeInstance === null) { //detect once on each trace
-					$oScopeInstance = false;
-					foreach (debug_backtrace(3) as $aTrace) {
-						if (!empty($aTrace['object']) && is_object($aTrace['object'])) {
-							$oScopeInstance = $aTrace['object'];
-							break;
-						}
+					if (empty(self::$aPrevTrace)) {
+						self::$aPrevTrace = debug_backtrace(1, 3);
+						array_shift(self::$aPrevTrace); //shif this current level
 					}
+					$oScopeInstance = self::$aPrevTrace[1]['object'] ?? false;
 				}
 				if ($oScopeInstance) {
 					if ($oScopeInstance instanceof Throwable) {
@@ -179,15 +252,7 @@ class AfrEvent
 					if ($newBoundClosure instanceof Closure) {
 						$closure = $newBoundClosure;
 					} else {
-						$bFatal = false;
-						if (Afr::app()) {
-							try {
-								$bFatal = Afr::app()->env()->getEnv('AFR_EVENT_BIND_EXCEPTION_IS_FATAL_ERROR', true);
-							} catch (Throwable $e) {
-								$bFatal = true;
-							}
-						}
-						if ($bFatal) {
+						if (Afr::app() && self::getEnv('AFR_EVENT_EXCEPTION_IS_FATAL_ERROR', true)) {
 							unset($aClosureAndScopeBound[0]);
 							throw new AfrEventException(
 								'Unable to bind #index.' . $i . ' from event(' . $sEvent . ', ' . $aData[self::X_EVT] .
@@ -206,16 +271,98 @@ class AfrEvent
 	}
 
 
-	protected static function autoEventName(int $iStackLevel = 3): string
+	protected static function autoEventName(int $iStackLevel, array $aEvents = null): string
 	{
-		$last = $iStackLevel - 1;
-		$prev = $iStackLevel - 2;
-		$aEvents = debug_backtrace(DEBUG_BACKTRACE_PROVIDE_OBJECT, $iStackLevel);
-		if (!empty($aEvents[$last]['class']) && !empty($aEvents[$last]['function'])) {
-			return array_slice(explode('\\', $aEvents[$last]['class']), -1, 1)[0] .
-				'.' . $aEvents[$last]['function'];
+		if (!empty($aEvents[$iStackLevel - 1]['class']) && !empty($aEvents[$iStackLevel - 1]['function'])) {
+			return array_slice(explode('\\', $aEvents[$iStackLevel - 1]['class']), -1, 1)[0] .
+				'.' . $aEvents[$iStackLevel - 1]['function'];
 		}
-		return basename($aEvents[$prev]['file']) . '.L' . $aEvents[$prev]['line'];
+		return basename($aEvents[$iStackLevel - 2]['file']) . '.L' . $aEvents[$iStackLevel - 2]['line'];
+	}
+
+	protected static function getEnv(string $sKey, $mDefault = null, $mCatch = null)
+	{
+		$sKey = (string)preg_replace("/[^A-Za-z0-9_]/", '_', strtoupper($sKey));
+		if (Afr::app()) {
+			try {
+				return Afr::app()->env()->getEnv($sKey, $mDefault);
+			} catch (Throwable $e) {
+				if ($mCatch !== null) {
+					return $mCatch;
+				}
+			}
+		} elseif (!empty($_ENV[$sKey])) {
+			return $_ENV[$sKey];
+		}
+		return $mDefault;
+	}
+
+
+	/**
+	 * @param string $sEvent
+	 * @param string $sFlag
+	 * @return int|bool|null|mixed
+	 */
+	public static function getConfigFlag(string $sEvent, string $sFlag)
+	{
+		if (isset(self::$aDataCollectFlags[$sEvent][$sFlag])) {
+			return self::$aDataCollectFlags[$sEvent][$sFlag];
+		}
+		if (isset(self::$aDataCollectFlags[self::WILDCARD_STAR][$sFlag])) {
+			return self::$aDataCollectFlags[self::WILDCARD_STAR][$sFlag];
+		}
+		//parse wildcard settings
+		foreach (self::$aDataCollectFlags as $sDataCollectFlag => $aFlags) {
+			if (empty($aFlags[$sFlag])) {
+				continue;
+			}
+			if (strpos($sDataCollectFlag, self::WILDCARD_STAR) !== false) {
+				$aSections = explode(self::WILDCARD_STAR, $sDataCollectFlag);
+				$iSections = count($aSections);
+				foreach ($aSections as $i => $sSection) {
+					$iSl = strlen($sSection);
+					if ($iSl < 1) {
+						continue;
+					}
+					if ($i == 0) {
+						if (substr($sEvent, 0, $iSl) === $sSection) {
+							return $aFlags[$sFlag];
+						}
+					} elseif ($i == $iSections - 1) {
+						if (substr($sEvent, -$iSl, $iSl) === $sSection) {
+							return $aFlags[$sFlag];
+						}
+					} else {
+						if (strpos($sEvent, $sSection) !== false) {
+							return $aFlags[$sFlag];
+						}
+					}
+				}
+			}
+		}
+		return null;
+		// return self::$aDataCollectFlags[$sEvent][$sFlag] ?? self::$aDataCollectFlags[self::WILDCARD_STAR][$sFlag] ?? null;
+	}
+
+	public static function setConfigFlag(string $sEvent, string $sFlag, $bOnOrInt): void
+	{
+		if ($bOnOrInt) {
+			self::$aDataCollectFlags[$sEvent][$sFlag] = $bOnOrInt;
+		} elseif (!empty(self::$aDataCollectFlags[$sEvent][$sFlag])) {
+			unset(self::$aDataCollectFlags[$sEvent][$sFlag]);
+			if (empty(self::$aDataCollectFlags[$sEvent])) {
+				unset(self::$aDataCollectFlags[$sEvent]);
+			}
+		}
+	}
+
+	public static function extendConfigFlagsFromArray(array $aFlags): void
+	{
+		foreach ($aFlags as $sEvent => $aFlagVal) {
+			foreach ($aFlagVal as $sFlag => $bOn) {
+				self::setConfigFlag($sEvent, $sFlag, $bOn);
+			}
+		}
 	}
 
 
@@ -245,7 +392,7 @@ class AfrEvent
 			self::$aOnEventClosures[$sEvent][] = [$closure, $bInnerBoundScope, $newScope];
 		}
 
-		self::addWildcardEvent($sEvent);
+		self::addWildcardClosure($sEvent);
 
 	}
 
@@ -261,32 +408,44 @@ class AfrEvent
 
 	public static function getWildcardConfig(): array
 	{
-		return self::$aWildcardEvents;
+		return self::$aWildcardClosure;
 
 	}
 
 	/**
 	 * @param int|null $iTraceDepth
-	 * @param string $sEnvTraceLimitKey
+	 * @param string $sEvent
 	 * @return int
 	 */
-	protected static function getTraceDepth(?int $iTraceDepth, string $sEnvTraceLimitKey): int
+	protected static function getTraceDepth(?int $iTraceDepth, string $sEvent): int
 	{
-		if (is_null($iTraceDepth)) {
-			$iTraceDepth = self::$iTraceDepth;
-			if (Afr::app()) {
-				try {
-					$iTraceDepth = max(
-						(int)Afr::app()->env()->getEnv($sEnvTraceLimitKey, $iTraceDepth),
-						$iTraceDepth
-					);
-				} catch (Throwable $e) {
-				}
-			} elseif (!empty($_ENV[$sEnvTraceLimitKey])) {
-				$iTraceDepth = max((int)$_ENV[$sEnvTraceLimitKey], $iTraceDepth);
-			}
+		if (!Afr::app()) { //app not booted
+			return is_null($iTraceDepth) ? self::$iDefaultTraceDepth : $iTraceDepth;
 		}
-		return (int)$iTraceDepth;
+
+		if (is_integer($cFlagVal = self::getConfigFlag($sEvent, self::X_TRACE))) {
+			return $cFlagVal; //can overwrite any other setting
+		}
+		if (static::getEnv('AFR_EVENT_X_TRACE', false)) {
+			if (($cFlagVal = (int)static::getEnv('AFR_EVENT_TRACE_DEPTH_' . $sEvent, -1, -1)) > -1) {
+				return $cFlagVal; //also, can overwrite any other code setting, Eg: AFR_EVENT_TRACE_DEPTH_SOME_XYZ_EVENT = 15
+			}
+			return is_null($iTraceDepth) ? static::getEnv('AFR_EVENT_DEFAULT_TRACE_DEPTH', 0) : $iTraceDepth;
+		}
+		return 0;
+	}
+
+
+	protected static function logArgs(string $sEvent): bool
+	{
+		if (!Afr::app() || self::getConfigFlag($sEvent, self::X_ARGS)) {
+			return true;
+		}
+		if (
+			static::getEnv('AFR_EVENT_X_ARGS_' . $sEvent, false, false)) {
+			return true; //also, can overwrite any other code setting, Eg: AFR_EVENT_X_ARGS_SOME_XYZ_EVENT = true
+		}
+		return false;
 	}
 
 	/**
@@ -298,7 +457,7 @@ class AfrEvent
 	{
 		$aTrace = [];
 		foreach ($aDebugBacktrace as $aInfo) {
-			$file = strtr($aInfo['file'] ?? '[PHP_Kernel]', '\\', '/');
+			$file = strtr($aInfo['file'] ?? '[PHP.Kernel⚙️]', '\\', '/');
 			if ($bSlim) {
 				$file = implode(
 					'/',
@@ -320,30 +479,25 @@ class AfrEvent
 		return $aTrace;
 	}
 
-	/**
-	 * @return float
-	 */
-	protected static function hrMicroTime(): float
-	{
-		static::$bHrTime ??= function_exists('hrtime');
-		return static::$bHrTime ? (hrtime(true) / 1e+6) : microtime(true);
-	}
 
 	/**
 	 * @param string $sEvent
 	 * @return void
 	 */
-	protected static function addWildcardEvent(string $sEvent): void
+	protected static function addWildcardClosure(string $sEvent): void
 	{
-		if (empty(self::$aWildcardEvents[$sEvent]) && substr_count($sEvent, self::WILDCARD_STAR) > 0) {
+		if (empty(self::$aWildcardClosure[$sEvent]) && substr_count($sEvent, self::WILDCARD_STAR) > 0) {
 			$aMap = [];
 			if ($sEvent === self::WILDCARD_STAR) { //*
-				self::$aWildcardEvents[$sEvent] = $aMap[self::WILDCARD_STAR] = true; //match any event
+				self::$aWildcardClosure[$sEvent] = $aMap[self::WILDCARD_STAR] = true; //match any event
 			} else {
 				$aSections = explode(self::WILDCARD_STAR, $sEvent);
 				$iSections = count($aSections);
 
 				foreach ($aSections as $i => $sSection) {
+					if (strlen($sSection) < 1) {
+						continue;
+					}
 					if ($i === 0) {
 						$aMap[self::WILDCARD_STARTS_WITH] = [$sSection, strlen($sSection)];
 					} elseif ($i === $iSections - 1) {
@@ -356,7 +510,7 @@ class AfrEvent
 					}
 				}
 			}
-			self::$aWildcardEvents[$sEvent] = $aMap;
+			self::$aWildcardClosure[$sEvent] = $aMap;
 		}
 	}
 }
